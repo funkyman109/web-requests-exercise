696780560ae88d3217aa5bd5959ea4ba82c31132
funkyman109 <59669523+funkyman109@users.noreply.github.com>
2020-02-12 17:41:54 -0500

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,1 +1,13 @@
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+import requests 
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/2.json"
+print(request_url)
+
+response = requests.get(request_url)
+print(type(response))
+print(dir(response))
+
+print(response.status_code)
+print(response.text)
